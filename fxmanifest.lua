3dad8b80a06ee4a1abd03a04fbb17ea01212378e
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-05-06 15:00:18 +1000

    Fix Ped Fleeing & High Resmon
    Moved ped health checks to the server, and fixed the ped fleeing when reaching a percentage of ped deaths.
    
    Optimisations for listening to player shots for drivers, reducing the resmon per ped.

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DonHulieo'
+description 'Ped Patrols for QBCore'
+version '1.1.5'
+
+shared_scripts {'config.lua', 'lib.lua'}
+
+client_script {'client/main.lua'}
+
+server_script {'server/main.lua'}
+
+escrow_ignore {'config.lua'}
+
+lua54 'yes'
